720101e2b01452d92a20233f8bb31339949109fa
JunTran304 <Tranquangminh.304@gmail.com>
2015-12-11 10:11:06 +0700

    Source code webpc

---
+++ shoppc/include/footer.php
@@ -0,0 +1,8 @@
+<embed id="Advertisement" 
+width="960" height="100" 
+flashvars="clickTARGET=_blank&clickTAG=#" 
+allowscriptaccess="always"
+wmode="transparent" quality="high" 
+name="Advertisement" style="admincp" 
+src="images/quangcao.swf" 
+type="application/x-shockwave-flash">
